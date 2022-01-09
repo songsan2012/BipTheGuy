dc45fa3f4611d23454f4abeb02c323378c4f02a4
Song San <songsan2012@gmail.com>
2022-01-09 10:32:28 -0800

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by song on 1/9/22.
+//  Copyright © 2022 song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
